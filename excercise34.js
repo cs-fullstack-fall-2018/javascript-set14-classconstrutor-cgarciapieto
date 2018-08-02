ff5e62fe6dbab2ad6c34c00739777c6cc8189527
Carlos Garcia <cgarciapieto@gmail.com>
2018-08-02 15:52:07 -0500

    javascript-set14-classconstrutor-cgarciapieto.iml

---
+++ excercise34.js
@@ -0,0 +1,16 @@
+class ToDo{
+    contrusctor(name, dueDate, List)
+    {
+this.name = name;
+this.dueDate = dueDate;
+this.list = list;
+    }
+
+}
+
+function main(){
+    newToDoList = ToDo("Kenn", "Next Week", ["Do the dishes", "Wash your clothes"]);
+    newToDoList.log(printToDolist.list);
+}
+
+main();
